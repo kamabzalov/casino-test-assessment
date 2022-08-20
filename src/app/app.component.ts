f9f2e3dcb097017e594d942a308f97b751781a0e
Kamil Abzalov <mail@kamil-abzalov.com>
2022-08-20 22:18:33 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'casino-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'casino-assesment';
+}
