d601230317032164d47353d8b5718308dbceea0d
Kamil Abzalov <mail@kamil-abzalov.com>
2022-08-20 23:14:01 +0300

    Test linters and prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'casino-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'casino-assesment';
